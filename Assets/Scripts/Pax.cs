b5c1ee37d678b674496ddda4be47fa51a7b07a3d
wladweb <wladwebwork@gmail.com>
2021-04-14 22:27:49 +0300

    added passangers

---
+++ Assets/Scripts/Pax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Pax : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void OnEnable()
+    {
+        _animator = GetComponent<Animator>();
+
+        float offset = Random.Range(0, 1f);
+        _animator.SetFloat("Offset", offset);
+
+        int animIndex = Random.Range(1, 6);
+        _animator.SetFloat("Rand", animIndex);
+    }
+}
