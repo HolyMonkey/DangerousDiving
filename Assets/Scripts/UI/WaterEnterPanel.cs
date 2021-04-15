54f8f071e6e007dddf5b413a5ba6c045de5c6982
wladweb <wladwebwork@gmail.com>
2021-04-15 18:40:05 +0300

    changed end level ui panel

--- Assets/Scripts/UI/WaterEnterPanel.cs
+++ Assets/Scripts/UI/WaterEnterPanel.cs
@@ -4,18 +4,26 @@ using DG.Tweening;
 public class WaterEnterPanel : MonoBehaviour
 {
     [SerializeField] private RectTransform _innerPanel;
+    [SerializeField] private RectTransform[] _scorePanels;
     [SerializeField] private GameEvent _levelFinished;
+
 
     private void OnEnable()
     {
         _innerPanel.DOScale(Vector3.zero, 0.2f).From().SetEase(Ease.OutBack).SetDelay(.3f).OnComplete(() => 
         {
-            _innerPanel.DOScale(Vector3.zero, 0.2f).SetEase(Ease.InBack).SetDelay(.3f).OnComplete(() => 
+            Sequence sequence = DOTween.Sequence();
+
+            sequence.Append(_scorePanels[0].DOScale(Vector3.one, 0.4f).SetEase(Ease.OutBack).SetDelay(.2f));
+            sequence.Append(_scorePanels[1].DOScale(Vector3.one, 0.4f).SetEase(Ease.OutBack).SetDelay(.2f));
+            sequence.Append(_scorePanels[2].DOScale(Vector3.one, 0.4f).SetEase(Ease.OutBack).SetDelay(.2f).OnComplete(() =>
             {
                 gameObject.SetActive(false);
-            });
+            }));
         });
     }
+
+
 
     private void OnDisable()
     {
